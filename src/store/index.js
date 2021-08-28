eacc4aa05d070b02f5ece92aba69d3d458f51155
fquijada <fquijada@centribal.com>
2021-08-28 19:45:24 -0400

    agrego action fetch data

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,20 @@ export default createStore({
   //Para modificar el state
   //Las mutaciones se llaman a travez de commit
   mutations: {
-    setProducts(state, payload){
-        state.products = payload
+    setProducts({state, payload}){
+      state.products = payload
     }
   },
   actions: {
-
+    async fetchData({commit}){
+      try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        commit('setProducts', data);
+      }catch(error){
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
